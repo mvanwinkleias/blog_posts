a57277fea8af7b28c01c077a3b5891e7d3351c89
Martin VanWinkle III <mvanwinkle@ias.edu>
2023-10-06 07:55:14 -0400

    progress

--- 2023-08-02-url_uri_analysis/uri_parse2.pl
+++ 2023-08-02-url_uri_analysis/uri_parse2.pl
@@ -1,8 +1,19 @@
 #!/usr/bin/perl
 
-use Data::Dumper;
 use URI;
+use JSON;
 
+my $uri_subs = [
+	'scheme',
+	'opaque',
+	'path',
+	'fragment',
+	'as_string',
+	'as_iri',
+#	'canonical', Returns an object
+	'secure',
+	'has_recognized_scheme',
+];
 my $url_string = $ARGV[0];
 
 if (! defined $url_string )
@@ -13,8 +24,18 @@ if (! defined $url_string )
 
 my $uri = URI->new($url_string);
 
-my %query = $uri->query_form();
+my $data = {};
 
-# print $uri->opaque,$/;
-print $uri->host(),$/;
-print Dumper(\%query),$/;
+# $data->{host} = $uri->host();
+
+foreach my $uri_sub (@$uri_subs)
+{
+	# print "URI sub: $uri_sub\n";
+	$data->{$uri_sub} = $uri->$uri_sub();
+}
+
+$data->{query} = { $uri->query_form() };
+
+my $json = JSON->new->allow_nonref;
+
+print $json->encode($data),$/;
