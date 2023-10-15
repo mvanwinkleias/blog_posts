912229a5e7e95aeb8a5d7bddab5fe036020352a6
Martin VanWinkle III <mvanwinkle@ias.edu>
2023-10-14 20:42:18 -0400

    progress

---
+++ 2023-09-18-universal_project_directories/project_layout/src/cgi-bin/some_cgi_collection/another_cgi_program.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+cat << EOF
+Content-type: text/html
+
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<title>Hello World</title>
+</head>
+<body>
+Hello World
+</body>
+</html>
+EOF
+exit 0
+
