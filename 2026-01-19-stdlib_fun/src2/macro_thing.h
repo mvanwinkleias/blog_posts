be7c397c25e741badc183b2fb66f89c1d83ce3c4
Martin VanWinkle III <mvanwinkle@ias.edu>
2026-01-26 22:35:48 -0500

    progress

---
+++ 2026-01-19-stdlib_fun/src2/macro_thing.h
@@ -0,0 +1,18 @@
+
+#define stdlib_dynamic_cmp_name(DYNAMIC_TYPE) cmp_dynamic_type_ ## DYNAMIC_TYPE
+#define stdlib_dynamic_cmp_type(DYNAMIC_TYPE) \
+DYNAMIC_TYPE cmp_dynamic_type_ ## DYNAMIC_TYPE( const void * a , const void * b)\
+{\
+	DYNAMIC_TYPE ia = *(const DYNAMIC_TYPE *) a;\
+	DYNAMIC_TYPE ib = *(const DYNAMIC_TYPE *) b;\
+\
+	if (ia < ib)\
+	{\
+		return -1;\
+	}\
+	else if (ia > ib)\
+	{\
+		return 1;\
+	}\
+	return 0;\
+}
---
+++ 2026-01-19-stdlib_fun/src2/test_dynamic_type.c
@@ -0,0 +1,8 @@
+#include "macro_thing.h"
+
+stdlib_dynamic_cmp_type(int)
+
+int main(int argc, char * argv[])
+{
+	return 0;
+}
